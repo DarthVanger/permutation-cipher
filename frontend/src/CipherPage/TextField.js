81faf26b735b6dbcbfff5b3b0d44c6f62d8cd551
DarthVanger <darthvanger@gmail.com>
2022-04-06 22:15:40 +0300

    Improve error message styling

--- frontend/src/CipherPage/TextField.js
+++ frontend/src/CipherPage/TextField.js
@@ -3,8 +3,8 @@ import './TextField.css';
 const TextField = (props) => {
   const { label, error, ...rest } = props;
   return (
-    <>
-      <label className="text-field">{label}
+    <div className="text-field">
+      <label className="text-field-label">{label}
         <input type="text" {...rest} />
       </label>
 
@@ -13,7 +13,7 @@ const TextField = (props) => {
           {error}
         </div>
       )}
-    </>
+    </div>
   );
 };
 
