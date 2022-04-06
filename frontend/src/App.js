20a6be14f668d7ce5c2e035c95532b3f9cf9b7ac
DarthVanger <darthvanger@gmail.com>
2022-04-06 17:40:27 +0300

    Init create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CipherPage from './CipherPage/CipherPage';
+
+function App() {
+  return (
+    <div className="App">
+      <CipherPage />
+    </div>
+  );
+}
+
+export default App;
