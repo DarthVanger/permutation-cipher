a6368c6832743e3b25aefd1e2c879c9268942155
DarthVanger <darthvanger@gmail.com>
2022-04-07 03:38:15 +0300

    Handle errors on backend

--- PermutationCipherServer/server.py
+++ PermutationCipherServer/server.py
@@ -1,8 +1,8 @@
 from http.server import SimpleHTTPRequestHandler
 import socketserver
 import json
-from PermutationCipherServer.crypto import permutation_encrypt
 import os
+from .crypto import permutation_encrypt
 
 def start_server(port, static_files_dir):
 
@@ -12,23 +12,39 @@ def start_server(port, static_files_dir):
     allowed_origin = 'http://localhost:3000'
 
     class CipherHttpRequestHandler(SimpleHTTPRequestHandler):
-        def handleEncryptPassword(self):
-            body_string = self.rfile.read(int(self.headers['Content-Length']))
-            body = json.loads(body_string)
-            encrypted_password = permutation_encrypt(body['password'], body['encryptionKey'])
-
-            self.send_response(200)
-            self.send_header('Content-Type', 'application/json')
-            self.send_header('Access-Control-Allow-Origin', allowed_origin)
+        def send_internal_error(self):
+            self.send_response(500)
+            self.send_header('Content-type', 'text/html')
             self.end_headers()
+            self.wfile.write(bytes('Internal Server Error\n', 'utf8'))
 
-            self.wfile.write(bytes(json.dumps(encrypted_password), 'utf8'))
+        def send_not_found(self):
+            self.send_response(404)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(bytes('Not found\n', 'utf8'))
 
-            return
+        def handleEncryptPassword(self):
+            try:
+                body_string = self.rfile.read(int(self.headers['Content-Length']))
+                body = json.loads(body_string)
+                encrypted_password = permutation_encrypt(body['password'], body['encryptionKey'])
+
+                self.send_response(200)
+                self.send_header('Content-Type', 'application/json')
+                self.send_header('Access-Control-Allow-Origin', allowed_origin)
+                self.end_headers()
+
+                self.wfile.write(bytes(json.dumps(encrypted_password), 'utf8'))
+
+            except Exception:
+                self.send_internal_error()
 
         def do_POST(self):
             if self.path == '/encrypt-password':
                 return self.handleEncryptPassword()
+
+            self.send_not_found()
 
         # Enable CORS for local development with webpack dev server.
         # Not sure about security :)
